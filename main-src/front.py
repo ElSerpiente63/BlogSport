53ac005637ef40a862c14cdfc4bfefda0a9c56c7
A1000 <ayoub_semsar@yahoo.com>
2023-07-21 15:42:34 +0200

    [front]
    itération des containers

--- main-src/front.py
+++ main-src/front.py
@@ -36,34 +36,44 @@ class LoginAdmin(UserControl):
             ]
         )
 
+
+
+
 class MainFront(UserControl):
     def build(self):
         self.column_append = ft.Column(scroll="always",expand=True, alignment=ft.MainAxisAlignment.SPACE_EVENLY,controls=[])
-        self.__row__ = ft.Row(expand=True, alignment=ft.MainAxisAlignment.CENTER, vertical_alignment=ft.CrossAxisAlignment.START, wrap=False, controls=[self.column_append])
+        #self.__row__ = ft.Row(expand=True,alignment=ft.MainAxisAlignment.CENTER, vertical_alignment=ft.CrossAxisAlignment.START, wrap=False, controls=[self.column_append])
+        self.card = ft.Card(elevation=10, surface_tint_color=ft.colors.LIGHT_GREEN, content=ft.Container(width=400, height=700, bgcolor=ft.colors.BLACK))
+        self._column = ft.Column(expand=True,scroll="always",alignment=ft.MainAxisAlignment.START, wrap=False, controls=[], horizontal_alignment=ft.CrossAxisAlignment.CENTER,spacing=50)
         self.view =  ft.View(
             "/home",
             bgcolor=ft.colors.WHITE,
             controls=[
-                ft.Column(expand=True, wrap=False, alignment=ft.MainAxisAlignment.START, controls=[ft.Row(alignment=ft.MainAxisAlignment.CENTER, vertical_alignment=ft.CrossAxisAlignment.CENTER, controls=[ft.Container(padding=40, content=ft.Text('Blogsport'))]), ft.Row(alignment=ft.MainAxisAlignment.END, vertical_alignment=ft.CrossAxisAlignment.START, controls=[ft.Container(height=200,alignment=ft.alignment.top_left,padding=padding.only(right=20, bottom=50),content=ft.TextButton('Login', on_click=lambda e: e.page.go('/login')))])]),
-                self.__row__
+                ft.Column(expand=True, wrap=False, alignment=ft.MainAxisAlignment.START, controls=[
+                    ft.Row(alignment=ft.MainAxisAlignment.CENTER, vertical_alignment=ft.CrossAxisAlignment.CENTER, controls=[ft.Container(padding=padding.only(bottom=40, top=40 ),content=ft.Text('Blogsport', size=18, weight="bold")), ft.Row(expand=True, wrap=False, alignment=ft.MainAxisAlignment.END, vertical_alignment=ft.CrossAxisAlignment.CENTER, controls=[ft.TextButton('Login')])]),
+                    ft.Row(expand=True,wrap=False,controls=[self._column], alignment=ft.MainAxisAlignment.CENTER),
+                    ]),
             ])
         #on travaille d'abord avec une liste de titres
         self.response = requests.get('http://127.0.0.1:4001/articles')
         if self.response.status_code == 200:
             for i in range(len(self.response.json())):
-                self.column_append.controls.append(ft.Container(
-                    width=600,
-                    height=200,
+                self._column.controls.append(ft.Card(elevation=5,content=ft.Container(
+                    width=900,
+                    height=150,
                     content=ft.Text(str(self.response.json()[i]["title"]), weight="bold"),
                     alignment=ft.alignment.top_center,
-                ))
+                    on_click=print(self.response.json()[i]["title"]),
+                    border_radius=5,
+                    bgcolor="#f2f2f2"
+                )))
         else:
             print("Status not done")
         return self.view
 
 #mettre le texte sur une ligne chaque 60 caractères, police 18
 class News(UserControl):
-    def build(self, id: str):
+    def build(self, title: str):
         self.content = requests.get("http://")
         return ft.View(
             title,
--- script-python/script.py
+++ script-python/script.py
@@ -1,14 +1,15 @@
-import requests
-import json
-import hashlib
-import couchdb
-import config
-from config import *
-database = couchdb.Database(f"http://{username}:{password}@127.0.0.1:5984/blogsportusers")
-password = "pythonmaster63"
-json = {"username":"Ayoub", "password":hashlib.md5(str.encode(password)).hexdigest()}
-if database.save(json):
-    print('Done')
-else:
-    print('not done')
+import ast
 
+# Chemin vers le fichier à analyser
+fichier = 'C:/Users/33769/AppData/Local/Programs/Python/Python310/lib/site-packages/flet/colors.py'
+
+# Analyse du code source du fichier
+with open(fichier, 'r') as file:
+    tree = ast.parse(file.read())
+
+# Récupération des noms des variables
+variables = [node.id for node in ast.walk(tree) if isinstance(node, ast.Name) and not isinstance(node.ctx, ast.Load)]
+
+print(variables.index("RED"))
+# Affichage des noms des variables
+print(variables)
