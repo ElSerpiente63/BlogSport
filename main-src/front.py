22075d33120a686ea154e36e3fabf0856a4151ba
A1000 <ayoub_semsar@yahoo.com>
2023-08-08 22:41:43 +0200

    [front]
    navigation

--- main-src/front.py
+++ main-src/front.py
@@ -4,19 +4,24 @@ import requests
 from requests import *
 import json
 import uuid
-import regex
+import regex as re
 import flet as ft
 import requests
 column_ref = ft.Ref[ft.Column]()
 textfield_username_ref = ft.Ref[ft.TextField]()
 textfield_password_ref = ft.Ref[ft.TextField]()
 def main(page: ft.Page): 
+    list(page.views)
     page.title = "BlogSport"
     page.bgcolor = ft.colors.WHITE
+    pattern = r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$'
     def route_change(route):
         page.views.clear()
         if page.route == "/home":
-            MainFront().build()
+            page.views.append(MainFront().build())
+        if re.match(pattern, str(route)):
+            page.views.append(News(route.replace("/", '')).build())
+        page.update()
     def view_pop(view):
         page.views.pop()
         page.go(page.views[-1].route)
@@ -36,9 +41,6 @@ class LoginAdmin(UserControl):
             ]
         )
 
-
-
-
 class MainFront(UserControl):
     def build(self):
         self.column_append = ft.Column(scroll="always",expand=True, alignment=ft.MainAxisAlignment.SPACE_EVENLY,controls=[])
@@ -57,10 +59,14 @@ class MainFront(UserControl):
         #on travaille d'abord avec une liste de titres
         self.response = requests.get('http://127.0.0.1:4001/articles')
         self.__list__ = []
-        def on_container_click(index):
+        def on_container_click(index, page: ft.Page):
             def click_handler(e):
+                print(self.response.json()[index])
                 print(self.response.json()[index]["title"])
-                News(self.response.json()[index]["id"],self.response.json()[index]["title"])
+                route = self.response.json()[index]["title"]
+                e.page.go(f"/{route}")
+                e.page.views.clear()
+                e.page.views.append(News(route).build(route, page, self.response.json()[index]["content"]))
             return click_handler
 
         if self.response.status_code == 200:
@@ -73,7 +79,7 @@ class MainFront(UserControl):
                     alignment=ft.alignment.top_center,
                     border_radius=5,
                     bgcolor=ft.colors.WHITE,
-                    on_click=on_container_click(i)  # Utilise la fonction intermédiaire ici
+                    on_click=on_container_click(i, ft.Page)  # Utilise la fonction intermédiaire ici
                 )
                 self._column.controls.append(ft.Card(elevation=5, content=self.__container__))
         else:
@@ -82,20 +88,22 @@ class MainFront(UserControl):
 
 #mettre le texte sur une ligne chaque 60 caractères, police 18
 class News(UserControl):
-    def __init__(self, ide: str,title: str):
-        self.title = title 
-        self.ide = ide
-        self.json = {"ide":self.ide}
-        self.response = requests.post('http://127.0.0.1:4001/content', json=self.json)
+    def build(self, title: str, page: ft.Page, text: str):
+        self.title = title
+        super().__init__()
+        self.response = requests.post('http://127.0.0.1:4001/content', json={"data":"data"})
         print(self.response.json())
-        self.create_view()
-
-    def create_view(self):
-        return ft.View(
+        page.bgcolor = ft.colors.WHITE
+        page.add(
+            ft.Text("something")
+        )
+        self.view = ft.View(
             f"/{self.title}",
+            bgcolor=ft.colors.BLACK,
             controls = [
-                ft.Text("Something", weight="bold")
+                ft.Column(expand=True, wrap=False, controls=[ft.Text("Something")])
             ]
-        )   
+        )
+        return self.view   
 flet.app(target=main, port=4000, view=ft.WEB_BROWSER)
 
