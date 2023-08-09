26cd116b6fdf0f148ebee9e32ca402a5e007fe5e
A1000 <ayoub_semsar@yahoo.com>
2023-08-09 12:05:29 +0200

    [api]
    update des test

--- api-server-fastapi/test.py
+++ api-server-fastapi/test.py
@@ -4,12 +4,14 @@ import requests
 import uuid
 import importlib.util
 import string
+import random as rand 
 module_spec = importlib.util.spec_from_file_location('config', 'C:/Users/33769/Desktop/config/config.py')
 module = importlib.util.module_from_spec(module_spec)
 module_spec.loader.exec_module(module)
 response = requests.get(f'http://{module.username}:{module.password}@127.0.0.1:5984/blogsportarticles/_design/title/_view/title')
 database = couchdb.Database(f"http://{module.username}:{module.password}@127.0.0.1:5984/blogsportarticles")
-
+char = list(string.ascii_letters) + list(string.ascii_letters.upper())
+print(char)
 print(response.json())
 __list__ = []
 for i in range(len(response.json()['rows'])):
@@ -17,10 +19,19 @@ for i in range(len(response.json()['rows'])):
 
 for i in range(len(__list__)):
     id_doc = __list__[i]
-    key = 'title'
+    key = 'content'
     document = database[id_doc]
-    document[key] = str(uuid.uuid4())
-    database[id_doc] = document  
+    _text_ = []
+    for i in range(800):
+        _text_.append(char[rand.randint(0, len(char)-1)])
+    document[key] = "".join(_text_)
+    database[id_doc] = document
+for i in range(len(__list__)):
+    id_doc = __list__[i]
+    key = 'ide'
+    document = database[id_doc]
+    document[key] = str(uuid.uuid4()) 
+    database[id_doc] = document 
 res = requests.get(f"http://{module.username}:{module.password}@127.0.0.1:5984/blogsportarticles/_design/title/_view/title")
 print(res.json())
 print('Done')
